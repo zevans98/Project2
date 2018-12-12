74ee39fc99055fa78d49fd31d738f9d9573ee529
nikohofmann <41592016+nikohofmann@users.noreply.github.com>
2018-12-11 23:29:41 -0700

    Updated references to remove example, added user-specific functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,25 +2,28 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  app.get("/api/foods", function(req, res) {
+    db.Food.findAll({
+      where: {
+        userId: req.user.id
+      }
+    }).then(function(dbFoods) {
+      res.json(dbFoods);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  app.post("/api/foods", function(req, res) {
+    req.body.userId = req.user.id;
+    db.Food.create(req.body).then(function(dbFood) {
+      res.json(dbFood);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
+  app.delete("/api/foods/:id", function(req, res) {
+    db.Food.destroy({ where: { id: req.params.id } }).then(function(dbFood) {
+      res.json(dbFood);
     });
   });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,17 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.Food.findAll({}).then(function(dbFoods) {
       res.render("index", {
         msg: "Welcome!",
-        examples: dbExamples
+        entry: dbFoods
       });
     });
   });
 
   // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.get("/entry/:id", function(req, res) {
+    db.Food.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
         example: dbExample
       });
