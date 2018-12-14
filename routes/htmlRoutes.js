8338b1d6a6b3554c69b2d78840add5262a3e1dcc
nikohofmann <41592016+nikohofmann@users.noreply.github.com>
2018-12-13 19:49:59 -0700

    Removed console log for entries page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,7 +15,6 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/entry/:id", isLoggedIn, function(req, res) {
     db.Food.findOne({ where: { id: req.params.id } }).then(function(dbFoods) {
-      console.log(dbFoods.dataValues);
       res.render("entry", {
         entry: dbFoods.dataValues
       });
